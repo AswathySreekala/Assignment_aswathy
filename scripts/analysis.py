ef3c01483cd09e3750dcd3922c00681f443528c7
AswathySreekala <sreekumarannair_aswa@phd.ceu.edu>
2024-11-10 13:50:28 +0100

    Added Python script and data files

---
+++ scripts/analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the dataset
+data = pd.read_csv('/Users/aami/Documents/assignment_aswathy/data/clean/wfh_tidy_person_p.csv')
+
+# Display first 5 rows of the dataset
+print(data.head())
